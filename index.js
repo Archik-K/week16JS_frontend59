6b196dcea9ea346d053f2579243cffd2769fbcf9
Archik-K <ar.kryatova@yandex.ru>
2023-08-21 22:14:26 +0300

    js

--- index.js
+++ index.js
@@ -1,19 +1,14 @@
 const form_ = document.forms.formOne;
 const button_sending = document.querySelector("#sending");
-const inputs_ = document.forms.formOne.querySelector("input");
-const checkboxes = document.forms.formOne.querySelector(
-	'input[type="checkbox"]'
-);
+
+const checkboxes = document.forms.formOne.querySelector("#checkbox");
 let errors = [];
 
 form_.addEventListener("submit", function (event) {
 	event.preventDefault();
 });
 
-inputs_.forEach(function (input) {
-	for (let input of inputs) {
-		validity_of_the_form(input);
-	}
+function checkValidity(input) {
 	let validity = input.validity;
 	if (!validity.patternMismatch) {
 		validity.classList.add("error");
@@ -36,26 +31,32 @@ inputs_.forEach(function (input) {
 			errors.validationMessage + min;
 		validity.style.border = "1px solid red";
 	}
-});
-if (!checkboxes.checked) {
-	checkboxes.classList.add("error");
-	document.querySelector("#age_error").innerHTML = errors.validationMessage;
+}
+
+function checkAll() {
+	const inputs = document.forms.formOne.querySelector("input");
+	for (let input of inputs) {
+		checkValidity(input);
+	}
+	if (checkboxes.checked) {
+		checkboxes.classList.remove("error");
+		document.querySelector("#age_error").textContent = "";
+	} else {
+		checkboxes.classList.add("error");
+		errorMessage.textContent = "Чекбокс не выбран";
+	}
 }
 
 function clearErrors() {
-	// Удаление классов ошибок у полей
-	let errorFields = document.getElementsByClassName("error");
-	while (errorFields.length > 0) {
-		errorFields[0].classList.remove("error");
-	}
+	document.querySelector("#age_error").textContent = "";
 
-	// Удаление классов ошибок у checkbox
-	let checkboxes = document.getElementsByTagName("input");
-	for (let i = 0; i < checkboxes.length; i++) {
-		if (checkboxes[i].type === "checkbox") {
-			checkboxes[i].classList.remove("error");
-		}
-	}
+	const inputs = document.forms.formOne.querySelectorAll("input");
+	inputs.forEach((input) => {
+		input.classList.remove("error");
+		input.style.border = "none";
+	});
+
+	checkboxes.classList.remove("error");
 }
 
 button_sending.addEventListener("click", function () {
